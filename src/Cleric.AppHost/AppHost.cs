88166b261544634c6fd3334e4dc7addda31128f2
Matt Stoltz <matthew.g.stoltz@gmail.com>
2025-09-21 14:30:29 -0400

    Add database project

--- src/Cleric.AppHost/AppHost.cs
+++ src/Cleric.AppHost/AppHost.cs
@@ -3,10 +3,13 @@ var builder = DistributedApplication.CreateBuilder(args);
 var sql = builder.AddSqlServer("sql")
     .WithLifetime(ContainerLifetime.Persistent);
 
-var db = sql.AddDatabase("Cleric"); 
+var db = sql.AddDatabase("Cleric");
+
+var dbUp = builder.AddProject<Projects.Cleric_Database>("cleric-database");
 
 builder.AddProject<Projects.Cleric_Web>("cleric-web")
     .WithReference(db)
-    .WaitFor(db);
+    .WaitFor(db)
+    .WaitFor(dbUp);
 
 builder.Build().Run();
